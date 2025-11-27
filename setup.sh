a166fde0cc70e967c120b1a671ec985adc637a7d
thenimas <thenimas@hotmail.com>
2025-11-27 09:51:43 -0500

    updated localization files

--- setup.sh
+++ setup.sh
@@ -306,11 +306,12 @@ echo "$HOST_NAME" > /etc/hostname
 hwclock --systohc
 
 # adding locale
+echo "en_CA.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_CA.UTF-8
+export LC_ALL=en_CA.UTF-8
 
 wget https://github.com/thenimas/archmas-installer/raw/main/configs/keyboard -O /etc/default/keyboard
 wget https://github.com/thenimas/archmas-installer/raw/main/configs/mirrorlist -O /etc/pacman.d/mirrorlist

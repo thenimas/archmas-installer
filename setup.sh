0000a492fd38b8b809e049c7f54152a9a356e954
thenimas <thenimas@hotmail.com>
2026-08-07 00:43:18 -0400

    fixed laptop errors - removed zram in favor of zswap

--- setup.sh
+++ setup.sh
@@ -338,9 +338,8 @@ wget https://raw.githubusercontent.com/thenimas/archmas-installer/main/assets/gr
 
 pacman -Syu --noconfirm
 
-pacman -S --noconfirm --needed accountsservice ark base-devel bc bluez cantarell-fonts dex dmenu dosfstools fail2ban fastfetch flatpak gamemode gdb git gnome-software gnome-themes-extra grub gvfs i3-wm i3blocks i3lock i3status ibus jdk-openjdk kate lightdm lightdm-gtk-greeter linux lshw lxappearance lxinput maim man-db network-manager-applet nodejs pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse playerctl plymouth python redshift rxvt-unicode sox syncthing systemsettings thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin timeshift ttf-inconsolata ttf-liberation ufw virt-manager vlc wget xclip xdg-desktop-portal xdotool zram-generator cryptsetup xwallpaper geeqie ntp lynis rkhunter lxqt-policykit ffmpegthumbnailer system-config-printer avahi linux-headers linux-lts-headers noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+pacman -S --noconfirm --needed accountsservice ark base-devel bc bluez cantarell-fonts dex dmenu dosfstools fail2ban fastfetch flatpak gamemode gdb git gnome-software gnome-themes-extra grub gvfs i3-wm i3blocks i3lock i3status ibus jdk-openjdk kate lightdm lightdm-gtk-greeter linux lshw lxappearance lxinput maim man-db network-manager-applet nodejs pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse playerctl plymouth python redshift rxvt-unicode sox syncthing systemsettings thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin timeshift ttf-inconsolata ttf-liberation ufw virt-manager vlc wget xclip xdg-desktop-portal xdotool cryptsetup xwallpaper geeqie ntp lynis rkhunter lxqt-policykit ffmpegthumbnailer system-config-printer avahi linux-headers linux-lts-headers noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 
-wget https://github.com/thenimas/archmas-installer/raw/main/configs/zram-generator.conf -O /etc/systemd/zram-generator.conf
 wget https://github.com/thenimas/archmas-installer/raw/main/configs/timeshift.json -O /etc/timeshift/timeshift.json
 wget https://github.com/thenimas/thebian-installer/raw/main/configs/jail.local -O /etc/fail2ban/jail.local
 
@@ -433,11 +432,11 @@ runuser "$USER_NAME" -c 'yes | yay -Scc'
 EOT
 
 if [ "$IS_LAPTOP" == 1 ]; then
-    sed -i 's/# bindsym XF86MonBrightness/bindsym XF86MonBrightness/g' /home/"$USER_NAME"/.config/i3/config
-    sed -i 's/# order += "battery all"/order += "battery all"/g' /home/"$USER_NAME"/.config/i3/i3status.conf
+    sed -i 's/# bindsym XF86MonBrightness/bindsym XF86MonBrightness/g' /target/home/"$USER_NAME"/.config/i3/config
+    sed -i 's/# order += "battery all"/order += "battery all"/g' /target/home/"$USER_NAME"/.config/i3/i3status.conf
 
     arch-chroot /target /bin/bash << EOT
-pacman -S bluez bluez-utils iw powertop wpa_supplicant brightnessctl
+pacman -S --noconfirm bluez bluez-utils iw powertop wpa_supplicant brightnessctl
 EOT
 
     arch-chroot /target /bin/bash << EOT
